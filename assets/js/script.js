4496e75ef0443223abf39d9a0683822e50644e63
Imammulhaq <imam_chelsea2@apps.ipb.ac.id>
2021-11-10 00:07:24 +0700

    test drive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-  const countDate = new Date("November 16, 2021 00:00:00").getTime();
+  const countDate = new Date("November 10, 2021 00:12:00").getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
